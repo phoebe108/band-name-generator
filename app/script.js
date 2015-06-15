af0b19b412f481118da2aeee4a52f831a48628a9
Phoebe Merritt <pmerritt108@gmail.com>
2015-06-14 20:08:13 -0700

    Generate a band name from user-input.

--- app/script.js
+++ app/script.js
@@ -1,16 +1,49 @@
+'use strict';
+
 $('#submitWords').on('submit', function(e) { // register an event handler submits a form entry
   e.preventDefault(); // stop the page from refreshing when the form is submitted
 
-  var adjective = $('[name=adjective]').val(); // save user-inputed adjective (from the form) into an object
-  var adjPost;
+  var adjective = $('input[name=adjective]').val(); // save user-inputed adjective (from the form) into an object
+  var noun = $('input[name=noun]').val();
+  var verb = $('input[name=verb]').val();
 
-  // submit user-inputed adjective (from the form) to the server using a post request
+  var adjPost;
+  var nounPost;
+  var verbPost;
+
   if (adjective) {
-    adjPost = {word: adjective};
-    $.post('adjective', adjPost, function(response) {
+    adjPost = {word: adjective}; // save adjective as value of word property
+    $.post('adjective', adjPost, function(response) {   // submit user-inputed adjective (from the form) to the server using a post request
       var adjectiveRes = response.msg;
-      $('#adjectiveRes').text(adjectiveRes);
+      $('#adjectiveRes').text(adjectiveRes); // insert the message generated from the server response in the selected element
+    });
+  }
+  if (noun) {
+    nounPost = {word: noun};
+    $.post('noun', nounPost, function (response) {
+      var nounRes = response.msg;
+      $('#nounRes').text(nounRes);
+    });
+  }
+  if (verb) {
+    verbPost = {word: verb};
+    $.post('verb', verbPost, function (response) {
+      var verbRes = response.msg;
+      $('#verbRes').text(verbRes);
     });
   }
 });
+
+  $('#generateButton').click(function () {
+    var result = $('#bandNameResult');
+    $.get('/adjective', function (response) {
+      result.text(response.word);
+    });
+    $.get('/noun', function (response) {
+      result.append(' ' + response.word);
+    });
+    $.get('/verb', function (response) {
+      result.append(' ' + response.word);
+    });
+  });
 
--- server.js
+++ server.js
@@ -3,15 +3,15 @@ var app = express(); // store and return an application object (server)
 var port = process.env.PORT || 3000; // add port to listen on
 var bodyparser = require('body-parser');
 
-var getRandomWord = require('./lib/getRandomWord');
-var Adjective = require('./lib/adjective');
-var Noun = require('./lib/noun');
-var Verb = require('./lib/verb');
-var postWord = require('./lib/postWord');
+var getRandomWord = require('./lib/getRandomWord.js');
+var Adjective = require('./lib/adjective.js');
+var Noun = require('./lib/noun.js');
+var Verb = require('./lib/verb.js');
+var postWord = require('./lib/postWord.js');
 
 var adjective = new Adjective(); // create new instance object of Adjective class
-var noun = new Noun; // create new instance object of Noun class
-var verb = new Verb; // create new instance object of Verb class
+var noun = new Noun(); // create new instance object of Noun class
+var verb = new Verb(); // create new instance object of Verb class
 
 app.use(bodyparser.json()); // use the bodyparser module
 app.use(bodyparser.urlencoded({extended: true})); // give bodyparser module some settings
@@ -22,6 +22,12 @@ app.get('/', function (req, res) {
   res.sendFile('/app/index.html');
 });
 
+// responds to the client with a random adjective in json format (a get route)
+app.get('/adjective', function (req, res) {
+  res.json(getRandomWord(adjective));
+});
+
+// create a post route for adjectives: run the postWord function using the adjective sent from the client and the adjective object
 app.post('/adjective', function (req, res) {
   var word = postWord(req.body.word, adjective);
   res.json(word);
@@ -32,10 +38,22 @@ app.get('/noun', function (req, res) {
   res.json(getRandomWord(noun));
 });
 
+// create a post route for nouns: run the postWord function using the noun sent from the client and the noun object
+app.post('/noun', function (req, res) {
+  var word = postWord(req.body.word, noun);
+  res.json(word);
+});
+
 // responds to the client with a random verb in json format (a get route)
 app.get('/verb', function (req, res) {
   res.json(getRandomWord(verb));
 })
+
+// create a post route for verbs: run the postWord function using the verb sent from the client and the verb object
+app.post('/verb', function (req, res) {
+  var word = postWord(req.body.word, verb);
+  res.json(word);
+});
 
 // start a server on local machine at port 3000
 app.listen(port, function () {
